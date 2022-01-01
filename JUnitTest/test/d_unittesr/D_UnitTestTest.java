b32d52f99273e9d64587d9628badcfac4103c456
UsseinOla <77444770+UsseinOla@users.noreply.github.com>
2022-01-01 15:09:48 -0500

    Update D_UnitTestTest.java

--- JUnitTest/test/d_unittesr/D_UnitTestTest.java
+++ JUnitTest/test/d_unittesr/D_UnitTestTest.java
@@ -17,9 +17,10 @@ import org.junit.BeforeClass;
  *
  * @author User
  */
-public class D_UnitTesrTest {
-
-    public D_UnitTesrTest() {
+public class D_UnitTesrTest
+{
+    public D_UnitTesrTest()
+    {
     }
 
     @BeforeClass
@@ -28,63 +29,52 @@ public class D_UnitTesrTest {
          System.out.println("Before Class");
          }
     
-    
     D_UnitTesr helper;
+    
      @Before
     public void setup()
     {
-        //create helper object before test
-        
     helper= new D_UnitTesr();
-    
     }
-    
     
     /**
      * Test of SquareArea method, of class D_UnitTesr.
      */
     @Test
-    public void testSquareArea() {
+    public void testSquareArea() 
+    {
        D_UnitTesr obj=new D_UnitTesr();
        
        double actuall=obj.squareArea(2);
        
        double expected=4;
        
-       assertEquals(expected,actuall,0.00001);
-        
+       assertEquals(expected,actuall,0.00001);    
     }
 
     /**
      * Test of subtractTech method, of class D_UnitTesr.
      */
     @Test
-    public void testSubtractTech() {
-        
+    public void testSubtractTech() 
+    {
         D_UnitTesr ts=new D_UnitTesr();
-        
         double expected =ts.subtractTech(5, 5);
         assertTrue(expected==0);
-        
         double actuall =0;
-        
         assertEquals(expected,actuall,0.001);
-         
     }
     
     @Test
     public void test()
     {
     D_UnitTesr helper=new D_UnitTesr();
-    
    String actual= helper.truncateAInFirst2Positions("AACD");
    String expected ="CD";
    
    //AACD=>CD ACD=> CD CDEF=>CDEF CDAA=>CDAA
    assertEquals(expected,actual);
-
     }
-    
     
     @Test
     public void testTruncateAInFirst2Positions2()
@@ -93,17 +83,15 @@ public class D_UnitTesrTest {
     assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
     }
     
-    
     //ABCD=> false, ABAB=>true, AB=>TRUE, A=>false
     @Test
     public void testFunctionAreFirstAndLastTwoCharactersTheSame()
     {
-         D_UnitTesr helper=new D_UnitTesr();
+    D_UnitTesr helper=new D_UnitTesr();
     boolean actualValue=helper.areFirstAndLastTwoCharactersTheSame("ABCD");
     boolean expectedValue=false;
     assertEquals(expectedValue,actualValue);
     }
-    
     
     @Test
     public void testFunctionAreFirstAndLastTwoCharactersTheSame_NegativeSenario()
@@ -111,32 +99,26 @@ public class D_UnitTesrTest {
     assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
     }
     
-    
      @Test
     public void testFunctionAreFirstAndLastTwoCharactersTheSame_PositiveSenario()
     {
     assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
     }
    
-    
      @Test
        public void testSortArray()
        {
        int[] numbers={12,3,4,1};
        int[] expected={1,3,4,12};
-       
        Arrays.sort(numbers);
        assertArrayEquals(expected,numbers);
        }
     
-       
        @Test(expected=NullPointerException.class)
        public void testArraySort_NullArray()
        {
        int[] numbers=null;
-       Arrays.sort(numbers);
-       
-           
+       Arrays.sort(numbers);  
        }
     
     //check the time that array takes to finish the task
@@ -157,20 +139,9 @@ public class D_UnitTesrTest {
    System.out.println("After test");
    }
     
-   
    @AfterClass
    public static void afterClass()
    {
        System.out.println("After Class");
    }
-    
-   
-
-   
-   
-   
-   
-   
-   
-    
 }
